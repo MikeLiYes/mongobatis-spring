0b42d78831795e952c7e125d8a195577907702d5
lidebing <lidebing@qq.com>
2021-04-21 23:04:20 +0800

    add configration

--- src/main/java/com/github/mikeliyes/mongobatis/spring/annotation/ShellScannerConfigurer.java
+++ src/main/java/com/github/mikeliyes/mongobatis/spring/annotation/ShellScannerConfigurer.java
@@ -1,5 +1,22 @@
 package com.github.mikeliyes.mongobatis.spring.annotation;
 
-public class ShellScannerConfigurer {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+
+public class ShellScannerConfigurer implements BeanDefinitionRegistryPostProcessor{
+
+	@Override
+	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+		
+	}
+
+	@Override
+	public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+		
+	}
+	
+	
 
 }
